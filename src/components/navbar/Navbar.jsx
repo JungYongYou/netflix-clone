2bcf5297f03f21283f84d169aac3a767cf1d833d
jydeveloper <lukaryu@naver.com>
2022-02-14 16:11:41 +0900

    Navbar 디자인 적용

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import './navbar.scss';
+import { Search, Notifications, ArrowDropDown } from '@material-ui/icons';
+
+const Navbar = () => {
+	return (
+		<div className="navbar">
+			<div className="container">
+				<div className="left">
+					<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png" alt="" />
+					<span>Homepage</span>
+					<span>Series</span>
+					<span>Movies</span>
+					<span>New and Popular</span>
+					<span>My List</span>
+				</div>
+				<div className="right">
+					<Search className="icon" />
+					<span>KID</span>
+					<Notifications className="icon" />
+					<img src="https://bhent.co.kr/static/e96f6a3e03d16a6199bdd5191768f337/34081/%25EB%25B0%2595%25EC%25A7%2580%25ED%259B%2584_zhkzbp.jpg" alt="" />
+					<div className="profile">
+						<ArrowDropDown className="icon" />
+						<div className="options">
+							<span>Settings</span>
+							<span>Logout</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,10 @@
 import './home.scss';
-import { AcUnit } from '@material-ui/icons';
+import Navbar from '../components/navbar/Navbar';
 
 const Home = () => {
 	return (
 		<div className="home">
-			<AcUnit />
+			<Navbar />
 		</div>
 	);
 };
