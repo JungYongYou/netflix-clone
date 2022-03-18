398de2486939c3a2a1d13acb7081374cc9673506
jydeveloper <lukaryu@naver.com>
2022-03-18 15:19:53 +0900

    react-router-dom의 Redirect가 없어지고 Navigate replace to로 바뀌었다.

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
+	const user = false;
 	return (
 		<Router>
 			<Routes>
-				<Route exact path="/" element={<Home />} />
+				<Route path="/" element={user ? <Home /> : <Navigate replace to="/register" />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
 				<Route path="/movies" element={<Home type="movies" />} />
 				<Route path="/series" element={<Home type="series" />} />
 				<Route path="/watch" element={<Watch />} />
