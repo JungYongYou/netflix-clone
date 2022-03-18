7d756b312885059732ea2e3aff3f620c10e67973
jydeveloper <lukaryu@naver.com>
2022-03-18 15:02:02 +0900

    react-router-dom 추가, v6에 Switch가 Routes로 대체됨

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,19 @@ import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 const App = () => {
-	return <Login />;
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/movies" element={<Home type="movies" />} />
+				<Route path="/series" element={<Home type="series" />} />
+				<Route path="/watch" element={<Watch />} />
+			</Routes>
+		</Router>
+	);
 };
 
 export default App;
